ba0ef8b9e73d51abbf25cc78468ba251fdcac76e
valentin <vb.brebionpro@gmail.com>
2023-12-08 15:33:05 +0100

    ex 2: first routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import ReviewList from '../components/ReviewList'
+import Review from '../components/Review'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
-    return <>
-            <Layout>
-                <ReviewList />
-            </Layout>
-            
-    </>
+    return <BrowserRouter>
+                <Layout>
+                    <Routes>
+                        <Route path="/view" element={<Review/>} />
+                        <Route path="/reviewList" element={<ReviewList />} />
+                    </Routes>
+                </Layout> 
+            </BrowserRouter>
 }
 
 export default App
