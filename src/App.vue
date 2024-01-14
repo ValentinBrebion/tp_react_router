36aead3c34da9eff2f7e2b5732822937df41f138
Vincent Kraus <vincent.kraus@etudiant.univ-lr.fr>
2024-01-14 21:16:11 +0100

    base

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,38 @@
-<script setup>
-import { ref } from 'vue'
-import ProductItem from './components/ProductItem.vue'
-
-const products = ref([
-  {
-    name: 'Canapé jaune',
-    cover: './../assets/canape-jaune.png',
-    details: 'Livraison sous 2 semaines',
-    price: 799
-  },
-  {
-    name: 'Chaise bois',
-    cover: './../assets/chaise-bois.png',
-    details: 'Livraison sous 3 jours',
-    price: 299,
-    discountedPrice: 99
-  },
-  {
-    name: 'Fauteuil jaune',
-    cover: './../assets/fauteuil-jaune.png',
-    details: 'Rupture de stock',
-  }
-])
-</script>
+<script setup></script>
 
 <template>
   <main>
     <section class="list">
-      <ProductItem v-for="product in products" :key="product" :product="product"></ProductItem>
+      <div class="card">
+        <img src="./assets/canape-jaune.png" alt="canapé jaune" />
+        <div class="card-body">
+          <h2 class="card-title">Canapé jaune</h2>
+          <p>Livraison sous 2 semaines</p>
+          <div class="card-actions">
+            <button class="btn btn-primary">Commander</button>
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <img src="./assets/chaise-bois.png" alt="chaise bois" />
+        <div class="card-body">
+          <h2 class="card-title">Chaise bois</h2>
+          <p>Livraison sous 3 jours</p>
+          <div class="card-actions">
+            <button class="btn btn-primary">Commander</button>
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <img src="./assets/fauteuil-jaune.png" alt="fauteuil jaune" />
+        <div class="card-body">
+          <h2 class="card-title">Fauteuil jaune</h2>
+          <p>Rupture de stock</p>
+          <div class="card-actions">
+            <button class="btn btn-primary">Commander</button>
+          </div>
+        </div>
+      </div>
     </section>
   </main>
 </template>
